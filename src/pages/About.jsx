9d6e82f533c48611468633293bd648eef46e2aed
Xuân Lộc <nguyenxuanloc.02042001@gmail.com>
2025-03-03 16:25:50 +0700

    add

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router-dom";
+
+const About =()=>{
+    
+    return (
+        <>
+        <h1>About</h1>
+        <NavLink to={"/"}>Quay lai</NavLink>
+        </>
+    );
+};
+
+export default About;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router-dom";
+
+const Home =()=>{
+    
+    return (
+        <>
+        <h1>Home</h1>
+        <NavLink to={"/about"}>About</NavLink>
+        </>
+    );
+};
+
+export default Home;
